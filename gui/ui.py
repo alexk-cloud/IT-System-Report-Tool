aae1d9ab3d03f31e34f51b285c3d2fac32185c87
alexko-cloud <msg.alexko@gmail.com>
2026-02-14 18:40:49 -0800

    add instruction text and drop down menu for file type options

--- gui/ui.py
+++ gui/ui.py
@@ -17,14 +17,26 @@ def run_gui():
     root.title("System Report Tool")
     root.geometry(f"{window_width}x{window_height}+{x}+{y}")
     root.resizable(False, False)
-    
+
     root.configure(bg="#000")
 
     title = StringVar(value="System Report Tool")
+    title_label = Label(root, textvariable=title, bg="#111", fg="#fff", 
+          font=("Arial", 20, "bold"), height=2)
+    
+    select = StringVar(value="Select file type to generate:")
+    select_label = Label(root, textvariable=select, bg = "#000", fg="#fff", 
+          font=("Arial", 14, "bold"),
+          height=1)
+    
+    choice = StringVar()
+    options = [".txt",".csv"]
+    choice.set(".txt")
+    menu = OptionMenu(root, choice, *options)
 
-    Label(root, textvariable=title, bg="#111", fg="#fff", 
-          font=("Arial", 20, "bold"),
-          height=2).pack(fill="x", padx=10, pady=10)
+    title_label.pack(fill="x", padx=10, pady=10)
+    select_label.pack()
+    menu.pack(pady=10)
     
     # bind to generate later
     #root.bind("<Return>", None)
